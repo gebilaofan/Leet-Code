a563551c0ba2cadd83f9185217e1605880421a50
gebilaofan <gebilaofl@163.com>
2022-01-25 14:30:42 +0800

    ➕improvement(commit规范): commit 规范自定义模版

--- .cz-config.js
+++ .cz-config.js
@@ -2,27 +2,27 @@
 
 module.exports = {
   types: [
-    { value: '🚧WIP', name: 'WIP:            开发中' },
-    { value: '✨feat', name: 'feat:           一个新特性' },
-    { value: '➕improvement', name: 'improvement:    对现有特性的提升' },
-    { value: '🐛fix', name: 'fix:            修复Bug' },
+    { value: '🚧WIP', name: '🚧   WIP:            开发中' },
+    { value: '✨feat', name: '✨   feat:           一个新特性' },
+    { value: '➕improvement', name: '➕   improvement:    对现有特性的提升' },
+    { value: '🐛fix', name: '🐛   fix:            修复Bug' },
     {
       value: '🛠refactor',
-      name: 'refactor:       代码重构，注意和特性、重构区分开',
+      name: '🛠    refactor:       代码重构，注意和特性、重构区分开',
     },
-    { value: '📚 docs', name: 'docs:           变更文档' },
-    { value: '🏁test', name: 'test:           修改或添加测试文件' },
-    { value: '📝config', name: 'config:         修改或添加配置文件' },
+    { value: '📚 docs', name: '📚   docs:           变更文档' },
+    { value: '🏁test', name: '🏁   test:           修改或添加测试文件' },
+    { value: '📝config', name: '📝   config:         修改或添加配置文件' },
     {
       value: '💅style',
-      name: 'style:          修改格式，不影响功能，例如空格、代码格式等',
+      name: '💅   style:          修改格式，不影响功能，例如空格、代码格式等',
     },
-    { value: '📈perf', name: 'perf:           性能提升' },
-    { value: '🔧ci', name: 'ci:             修改ci相关配置、脚本等' },
-    { value: '⏪revert', name: 'revert:         回退版本' },
+    { value: '📈perf', name: '📈   perf:           性能提升' },
+    { value: '🔧ci', name: '🔧   ci:             修改ci相关配置、脚本等' },
+    { value: '⏪revert', name: '⏪   revert:         回退版本' },
     {
       value: '🗯chore',
-      name: 'chore:          杂务，不属于以上类型，例如run build、引入或更新软件包等',
+      name: '🗯    chore:          杂务，不属于以上类型，例如run build、引入或更新软件包等',
     },
   ],
 
